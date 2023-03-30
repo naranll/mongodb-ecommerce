f8dcb9cfc852c9832832b2822ed616381fcd22ba
Morning <morning@1-9.local>
2023-03-30 11:46:28 +0800

    deleted minor strings

--- routes/user-route.js
+++ routes/user-route.js
@@ -5,31 +5,30 @@ import User from "../models/User.js";
 const userRouter = express.Router();
 
 userRouter.post("/login", async (req, res) => {
-  try {
-    const { email, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({email});
-    if( user && await (bcrypt.compare(password, user.password))){
-        console.log("matched");
-        res.status(200).send({"message": "existing user, able to login"});
-    }else{
-        res.status(401).send({"message": "unauthorized, user password wrong"});
+        const user = await User.findOne({ email });
+        if (user && await (bcrypt.compare(password, user.password))) {
+            console.log("matched");
+            res.status(200).send({ "message": "existing user, able to login" });
+        } else {
+            res.status(401).send({ "message": "unauthorized, user password wrong" });
+        }
+
+    } catch (error) {
+        console.error(error);
     }
-
-  } catch (error) {
-    console.error(error);
-  }
 });
 
 userRouter.post("/register", async (req, res) => {
-  // console.log("pass", req.body.password);
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const newUser = new User({
-    email: req.body.email,
-    password: hashedPassword,
-  });
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+        email: req.body.email,
+        password: hashedPassword,
+    });
 
-  const result = await newUser.save();
+    const result = await newUser.save();
 });
 
 export default userRouter;
